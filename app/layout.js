923641b97f5d0a8d5079b7ded43c83546f8f36ca
ChrissTuruka <chrissjustice07@gmail.com>
2025-01-11 13:50:11 +0300

    First Commit

---
+++ app/layout.js
@@ -0,0 +1,15 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Lugha Tausi Portfolio",
+  description:
+    "Meet the team and people behind Lugha Tausi. A Revolutionary Kiswahili Programming Language",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. At quod nulla
+        quisquam modi suscipit omnis aliquid in accusantium, laudantium maiores?
+        Molestias molestiae officiis quaerat voluptates corporis. Asperiores
+        ducimus nam reiciendis.
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
